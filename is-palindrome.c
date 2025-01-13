bac3ea78c1cacb612e638acc4630c6c84304428d
Emre AĞAN <80820535+semre57@users.noreply.github.com>
2025-01-13 05:26:23 +0300

    is-palindrome

---
+++ is-palindrome.c
@@ -0,0 +1,54 @@
+//
+// Created by Emre Ağan on 13.01.2025.
+//
+#include <stdio.h>
+#include <ctype.h>
+
+#define SIZE 100
+
+void sgets(char *p) {
+    int c;
+    while ((c = getchar()) != '\n')
+        *p++ = c;
+    *p = '\0';
+}
+
+int main()
+{
+    char str [SIZE];
+    printf("Enter a string: ");
+    sgets(str);
+
+    int len;
+    for(len = 0; str[len]; len++) // str[len] means => (str[len] != '\0') also ('\0')'s ASCII is 0.
+        ; //null statement        // remember: in c language, non-zero values means true.
+
+    int l_ind = 0; // left index
+    int r_ind = len-1; // right index
+    int flag = 0;
+
+    char control[6] = {'!','?','.',',','\'',' '}; // we need to control elements of that array.
+
+    for(int i = 0; (i<len) && (l_ind<=r_ind); i++) {
+
+        for (int j=0; j<6; j++)
+
+            for(int k=0;k<6;k++) {
+
+                while(str[l_ind] == control[k])
+                    ++l_ind;
+
+                while(str[r_ind] == control[k])
+                    --r_ind;
+            }
+
+        if(tolower((int)str[l_ind++])  != tolower((int)str[r_ind--]))
+            flag++;
+
+    }
+
+    if (!flag)
+        printf("%s is palindrome!",str);
+    else
+        printf("%s is not palindrome.",str);
+}
